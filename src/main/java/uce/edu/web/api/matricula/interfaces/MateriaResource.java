616cceda7a58e4f3a962077f7e516a011971b8d9
KaiOxS <torito9926@hotmail.es>
2026-01-21 19:43:51 -0500

    Taller 28: Modelo de madurez de Richardson

--- src/main/java/uce/edu/web/api/matricula/interfaces/MateriaResource.java
+++ src/main/java/uce/edu/web/api/matricula/interfaces/MateriaResource.java
@@ -10,6 +10,7 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
 import uce.edu.web.api.matricula.application.MateriaService;
 import uce.edu.web.api.matricula.domain.Materia;
 
@@ -20,49 +21,49 @@ public class MateriaResource {
     private MateriaService materiaService;
 
     @GET
-    @Path("/todos")
+    @Path("")
     public List<Materia> ListarTodos() {
         return this.materiaService.ListarTodos();
     }
 
     @GET
-    @Path("/consultarPorId/{id}")
+    @Path("/{id}")
     public Materia consultarPorId(@PathParam("id") Integer iden) {
         return this.materiaService.consultarPorId(iden);
     }
 
     @POST
-    @Path("/crear")
+    @Path("")
     public void guardar(Materia mate) {
         this.materiaService.crear(mate);
     }
 
     @PUT
-    @Path("/actualizar/{id}")
+    @Path("/{id}")
     public void actualizar(@PathParam("id") Integer id, Materia mate) {
         this.materiaService.actualizar(id, mate);
     }
 
     @PATCH
-    @Path("/actualizarParcial/{id}")
+    @Path("/{id}")
     public void actualizarParcial(@PathParam("id") Integer id, Materia mate) {
         this.materiaService.actualizarParcial(id, mate);
     }
 
     @DELETE
-    @Path("/borrar/{id}")
+    @Path("/{id}")
     public void borrar(@PathParam("id") Integer id) {
         this.materiaService.eliminar(id);
     }
 
     @GET
-    @Path("/buscarPorNombre/{nombre}")
-    public List<Materia> buscarPorNombre(@PathParam("nombre") String nombre) {
+    @Path("/nombre")
+    public List<Materia> buscarPorNombre(@QueryParam("nombre") String nombre) {
         return this.materiaService.buscarPorNombre(nombre);
     }
 
     @GET
-    @Path("/codigoMayor/{codigo}")
+    @Path("/codigo")
     public List<Materia> buscarPorCodigoMayor(@PathParam("codigo") Long codigo) {
         return this.materiaService.buscarPorCodigoMayor(codigo);
     }
